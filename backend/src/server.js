47f8805ba40423c59b8e404dd5ff22163e2c44a7
vaib01232 <119183394+vaib01232@users.noreply.github.com>
2025-09-26 12:52:39 +0530

    init backend, added user and donation schemas

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+require ('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req,res) => res.send({ok: true, msg: 'Food_Link API'}));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
