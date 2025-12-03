f2fda9afeedfa1feba4229e998a9f1779868ac72
Vaibhaav BS <vaibhaavbs@Vaibhaavs-MacBook-Air.local>
2025-12-03 14:36:05 +0530

    deploying ? finally ?

--- frontend/food-link/src/components/PostDonation.jsx
+++ frontend/food-link/src/components/PostDonation.jsx
@@ -14,7 +14,6 @@ import {
   ArrowLeft
 } from "lucide-react";
 import { API_ENDPOINTS, BACKEND_BASE_URL } from '../config/api';
-import { GOOGLE_MAPS_API_KEY } from '../config/maps';
 import toast from 'react-hot-toast';
 import LocationPicker from './LocationPicker';
 import PhoneVerificationModal from './PhoneVerificationModal';
@@ -476,7 +475,7 @@ const PostDonationPage = () => {
                       onLocationSelect={handleLocationSelect}
                       initialLat={formData.lat}
                       initialLng={formData.lng}
-                      apiKey={GOOGLE_MAPS_API_KEY}
+                      apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
                     />
                   </div>
 
